8226ec85d580a2f83fc7ebf34a7753b65e29e2f4
Peter Son Struschka <me@peter-struschka.com>
2017-11-06 11:09:29 +0700

    setup database

---
+++ src/main/java/io/muic/designpattern/Main.java
@@ -0,0 +1,13 @@
+package io.muic.designpattern;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-------STARTED----------");
+        SpringApplication.run(Main.class, args);
+    }
+}
