d2a24c485a1023ce17a4b0d8f48ed8c34a678197
Dev Singh <dev.thep@hotmail.com>
2017-11-26 17:31:34 +0700

    game dest in socket controller

--- src/main/java/io/muic/designpattern/controllers/SocketController.java
+++ src/main/java/io/muic/designpattern/controllers/SocketController.java
@@ -19,6 +19,9 @@ public class SocketController {
     @Autowired
     SubscriberService subscriberService;
 
+    String player1 = "";
+    String player2 = "";
+
 //    @Autowired
 //    public SocketController(SimpMessagingTemplate template){
 //        this.template = template;
@@ -38,11 +41,50 @@ public class SocketController {
     @MessageMapping(value = "/msg/{id}")
     @SendTo("/sub/game")
     public Reply reply1(SimpMessageHeaderAccessor simpMessageHeaderAccessor, MyMessage message, @DestinationVariable long id) throws Exception {
-        System.out.println("Chess game player : " + message.getFrom());
-        System.out.println(message.getFrom() + " TEST " + id);
-        String s = subscriberService.getSubscribers().get(simpMessageHeaderAccessor.getSessionId());
-        return new Reply("In Game " + s);
+//        System.out.println("Chess game player : " + message.getFrom());
+//        System.out.println(message.getFrom() + " TEST " + id);
+//        String s = subscriberService.getSubscribers().get(simpMessageHeaderAccessor.getSessionId());
+//        return new Reply("In Game " + s);
 //        ChessGame chessGame= ChessGameService.findOne(id);
 //        .....
+        if (message.getCommand().equals("reset")) {
+            player1 = "";
+            player2 = "";
+            return new Reply("reset");
+        }
+        if (message.getCommand().equals("start")){
+            if (player1.equals("")){
+                player1 = message.getFrom();
+                System.out.println("player1 set");
+                Reply wait = new Reply("wait");
+                wait.setPlayer1(player1);
+                return wait;
+            }else {
+                player2 = message.getFrom();
+                Reply start = new Reply("start");
+                start.setPlayer1(player1);
+                start.setPlayer2(player2);
+                System.out.println("player2 set");
+                return start;
+            }
+        }
+        if (message.getCommand().equals("move")){
+            if (message.getFrom().equals(player1)){
+                Reply move = new Reply("switch",2,message.getFenBoard());
+                move.setSource(message.getSource());
+                move.setTarget(message.getTarget());
+                move.setPlayer1(player1);
+                move.setPlayer2(player2);
+                return move;
+            }else {
+                Reply move = new Reply("switch",1,message.getFenBoard());
+                move.setSource(message.getSource());
+                move.setTarget(message.getTarget());
+                move.setPlayer1(player1);
+                move.setPlayer2(player2);
+                return move;
+            }
+        }
+        return new Reply("Chess");
     }
 }
