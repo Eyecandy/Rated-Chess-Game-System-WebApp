defe49ec2b6d09484dcca07f06cac764cee3fab8
Dev Singh <dev.thep@hotmail.com>
2017-11-26 16:56:18 +0700

    2 player communication

--- src/main/java/io/muic/designpattern/configurations/WebSocketConfig.java
+++ src/main/java/io/muic/designpattern/configurations/WebSocketConfig.java
@@ -2,10 +2,7 @@ package io.muic.designpattern.configurations;
 
 import io.muic.designpattern.services.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.annotation.Order;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 
@@ -14,16 +11,12 @@ import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 
-import org.springframework.messaging.simp.user.SimpUserRegistry;
-import org.springframework.messaging.simp.user.UserDestinationResolver;
 import org.springframework.messaging.support.ChannelInterceptorAdapter;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import sun.security.acl.PrincipalImpl;
 
-import java.security.Principal;
 import java.util.List;
 
 @Configuration
@@ -33,10 +26,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Autowired
     SubscriberService subscriberService;
-
-
-
-
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -57,39 +46,26 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
         registration.setInterceptors(new ChannelInterceptorAdapter() {
             public Message<?> preSend(Message<?> message, MessageChannel channel) {
                 StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+                System.out.println("Sub ID : " + accessor.getSubscriptionId());
+//                System.out.println("Dest " + accessor.getDestination());
                 List<String> tokenList = accessor.getNativeHeader("user");
                 String token = null;
-
                 System.out.println(accessor.getCommand().getMessageType());
                 if (accessor.getCommand().getMessageType() == SimpMessageType.DISCONNECT) {
                     subscriberService.removeSession(accessor.getSessionId());
                 }
-
-
-
-
-
                 if (tokenList == null || tokenList.size() < 1) {
-
                     return message;
                 } else {
                     token = tokenList.get(0);
                     if (token == null) {
-
-
                         return message;
                     }
                 }
-
-
                 //Principal principal = new PrincipalImpl(tokenList.get(0));
                 accessor.setLeaveMutable(true);
-
                 //subscriberService.getSubscribers().put(accessor.getSessionId(),tokenList.get(0));
                 subscriberService.register(accessor.getSessionId(),tokenList.get(0));
-
-
-
                 return MessageBuilder.createMessage(message.getPayload(), accessor.getMessageHeaders());
             }
         });
--- src/main/java/io/muic/designpattern/controllers/SocketController.java
+++ src/main/java/io/muic/designpattern/controllers/SocketController.java
@@ -4,7 +4,7 @@ import io.muic.designpattern.model.MyMessage;
 import io.muic.designpattern.model.Reply;
 import io.muic.designpattern.services.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
@@ -18,9 +18,14 @@ public class SocketController {
 
     @Autowired
     SubscriberService subscriberService;
+
+//    @Autowired
+//    public SocketController(SimpMessagingTemplate template){
+//        this.template = template;
+//    }
+
     @MessageMapping("/msg")
     @SendTo("/sub/message")
-    @SubscribeMapping("sub/message/}")
     public Reply reply(MyMessage message,SimpMessageHeaderAccessor simpMessageHeaderAccessor) throws Exception {
         String s = subscriberService.getSubscribers().get(simpMessageHeaderAccessor.getSessionId());
         System.out.println(s + " HEY FROM ME");
@@ -28,5 +33,16 @@ public class SocketController {
             System.out.println(subscriberService.getSubscribers().values());
         }
         return new Reply(message.getFrom() + " YEYEY");
+    }
+
+    @MessageMapping(value = "/msg/{id}")
+    @SendTo("/sub/game")
+    public Reply reply1(SimpMessageHeaderAccessor simpMessageHeaderAccessor, MyMessage message, @DestinationVariable long id) throws Exception {
+        System.out.println("Chess game player : " + message.getFrom());
+        System.out.println(message.getFrom() + " TEST " + id);
+        String s = subscriberService.getSubscribers().get(simpMessageHeaderAccessor.getSessionId());
+        return new Reply("In Game " + s);
+//        ChessGame chessGame= ChessGameService.findOne(id);
+//        .....
     }
 }
